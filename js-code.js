35dcc779390cb1c2b7801bc0fd248f8b75e2d03d
kathiravan292 <kkathiravan785@gmail.com>
2025-06-24 15:56:30 +0530

    string literls and constructor

--- js-code.js
+++ js-code.js
@@ -1,18 +1,37 @@
 // var key word
 
 var code
-code="java script"
+code = "java script"
 document.write(code)
 
 
 // let key word
 
 let codefee
-codefee=2000;
+codefee = 2000;
 console.log(codefee)
 
 
 // const key word
 
-const mode="online or offline"
+const mode = "online or offline"
 console.log(mode)
+
+
+
+// primitive data type
+// string data type
+
+// string literals
+
+let courcedetail = "java script";
+console.log(courcedetail);
+console.log(typeof courcedetail);
+
+// string constructore
+
+let courcemode=new String('online or offline');
+console.log(courcemode);
+console.log(typeof courcemode);
+
+
