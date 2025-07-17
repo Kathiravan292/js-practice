fd1506ddafd455b079bd7e49cb20ef7dd5343e90
kathiravan292 <kkathiravan785@gmail.com>
2025-07-17 17:15:27 +0530

    test

--- regex.html/regex.js
+++ regex.html/regex.js
@@ -1,15 +1,22 @@
-let str = "apple apple apple";
-let result = str.match(/apple/);    // No flag
-console.log(result);                // Only ['apple']
+// let str = "apple apple apple";
+// let result = str.match(/apple/);    // No flag
+// console.log(result);                // Only ['apple']
 
-let result2 = str.match(/apple/g );  // With 'g' flag
-console.log(result2);
+// let result2 = str.match(/apple/g );  // With 'g' flag
+// console.log(result2);
 
 
-// //////////
-let str1 = "apple apple apple";
-let result1 = str.replace(/apple/);    
-console.log(result);                
+// // //////////
+// let str1 = "apple apple apple";
+// let result1 = str.replace(/apple/);    
+// console.log(result);                
 
-let result3 = str.replace(/apple/g,"banana" );  
-console.log(result3);
+// let result3 = str.replace(/apple/g,"banana" );  
+// console.log(result3);
+
+// ///////////
+               
+let str12 = / apple/;
+let result31 = str12.test(" i have a apple" );  
+console.log(result31);
+
