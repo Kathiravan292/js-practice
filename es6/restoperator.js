87087e410aaeb33fae94be2e54a94d604b84b57d
kathiravan292 <kkathiravan785@gmail.com>
2025-07-24 16:05:16 +0530

    call method

--- es6/restoperator.js
+++ es6/restoperator.js
@@ -1,34 +1,53 @@
-let names = ['Surya', 'Lokesh', 'harshith', 'Gayathri', 'Sandeep', 'Sanjay'];
+// let names = ['Surya', 'Lokesh', 'harshith', 'Gayathri', 'Sandeep', 'Sanjay'];
 
-let [eleOne, eleTwo, ...others] = names;
-console.log(eleOne);
-console.log(eleTwo);
-console.log(others);
+// let [eleOne, eleTwo, ...others] = names;
+// console.log(eleOne);
+// console.log(eleTwo);
+// console.log(others);
 
-function user(userOne, userTwo, ...restPara) {
-    console.log(userOne);
-    console.log(userTwo);
-    console.log(restPara);
+// function user(userOne, userTwo, ...restPara) {
+//     console.log(userOne);
+//     console.log(userTwo);
+//     console.log(restPara);
 
+// }
+// user('Adhithya', 'Jaswanth', 'Venkat', 'Pavan', 'Chakri', 'Kadhir')
+
+// // //////////////////////////////////
+// gexample:
+
+// let names = ['Surya','Lokesh','harshith','Gayathri','Sandeep'];
+// console.log(names);
+
+// // Older way of extracting all the values from an array / Without destructurig:
+// let resElem = names[0];
+// console.log(resElem); // 'Surya'
+// console.log(names[1]); // Lokesh
+
+// // //////////////////////////////
+// console.log(`********** With destructuring Array ************`);
+
+// let [eleOne,,,eleFour,eleFive] = names;
+
+// console.log(eleOne);
+// console.log(eleFour);
+// console.log(eleFive);
+
+// ///////////////////////////call()/////////////////////////////
+console.log(`********** With call method() ************`);
+let proces={
+    name : "kathir",
+    age : 21,
+    place : "nkl",
 }
-user('Adhithya', 'Jaswanth', 'Venkat', 'Pavan', 'Chakri', 'Kadhir')
 
-// //////////////////////////////////
-gexample:
-
-let names = ['Surya','Lokesh','harshith','Gayathri','Sandeep'];
-console.log(names);
-
-// Older way of extracting all the values from an array / Without destructurig:
-let resElem = names[0];
-console.log(resElem); // 'Surya'
-console.log(names[1]); // Lokesh
-
-// //////////////////////////////
-console.log(`********** With destructuring Array ************`);
-
-let [eleOne,,,eleFour,eleFive] = names;
-
-console.log(eleOne);
-console.log(eleFour);
-console.log(eleFive);
+let proces1={
+    name : "dhiva",
+    age : 21,
+    place : 'tri',
+}
+ let detail=function (month,year) {
+    console.log(` and name${this.name} and ${this.age} and${this.place}this is my month ${month} and this is${year}`);
+    
+ }
+ detail.call(proces1,2,2005)
